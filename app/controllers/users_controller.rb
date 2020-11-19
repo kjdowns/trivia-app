494ca7cfa0fbf0b2c12a2157736a10753d4292d9
kjdowns <kevin.downs04@gmail.com>
2020-11-19 17:23:15 -0500

    add userscontroller with stub actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    
+    def new
+        
+    end
+
+    def create
+        
+    end
+
+    def show
+    
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
