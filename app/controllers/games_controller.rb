691d38ff7f3261f0d8a84dd6f8db65aa7e396bf0
kjdowns <kevin.downs04@gmail.com>
2020-11-20 04:21:32 -0500

    add user_id to games model

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,7 @@
 class GamesController < ApplicationController
     
+    def show
+        
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :games
+
     has_secure_password
 
     validates :user_name, presence: true
